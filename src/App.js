9ef6ff5ed7a1fa9cfa4bd6ad239d66c0fd87634e
msoldner79 <131709621+msoldner79@users.noreply.github.com>
2024-03-17 15:22:28 -0500

    update inports for create.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import React, { useEffect, useReducer } from 'react';
 // import { API } from 'aws-amplify';
-import { List } from 'antd';
+import { List, Input, Button } from 'antd';
 import 'antd/dist/reset.css';
 import { listNotes } from './graphql/queries';
 import { generateClient } from 'aws-amplify/api';
+import { v4 as uuid } from 'uuid';
+import { createNote as CreateNote } from './graphql/mutations';
 
 const client = generateClient();
+
 
 const initialState = {
   notes: [],
